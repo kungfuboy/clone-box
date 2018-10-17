45ed2460eb48a651b25eb6667eed43ba2a2c8294
kungfuboy <17kungfuboy@gmail.com>
2018-10-17 20:44:51 +0800

    update

--- test/index.test.js
+++ test/index.test.js
@@ -1,51 +1,49 @@
-import test from 'ava'
-import clone from '../src/index'
+const clone = require('../src/index.js')
 
 const obj1 = {
-    a: 0,
-    b: {
-        i: 6,
-        j: {
-            k: {
-                f: '8'
-            }
-        }
-    },
-    h: 7
+  a: 0,
+  b: {
+    i: 6,
+    j: {
+      k: {
+        f: '8'
+      }
+    }
+  },
+  h: 9
 }
 const obj2 = {
-    a: 1,
-    b: {
-        c: {
-            d: 2,
-            e: {
-                f: 3,
-                g: [4, 5, 6]
-            }
-        }
+  a: 1,
+  b: {
+    c: {
+      d: 2,
+      e: {
+        f: 3,
+        g: [4, 5, 6, 7]
+      }
     }
+  }
 }
 const result = {
-    a: 1,
-    b: {
-        i: 6,
-        j: {
-            k: {
-                f: '8'
-            }
-        },
-        c: {
-            d: 2,
-            e: {
-                f: 3,
-                g: [4, 5, 6]
-            }
-        }
+  a: 1,
+  b: {
+    i: 6,
+    j: {
+      k: {
+        f: '8'
+      }
     },
-    h: 7
+    c: {
+      d: 2,
+      e: {
+        f: 3,
+        g: [4, 5, 6, 7]
+      }
+    }
+  },
+  h: 9
 }
 
-test('两个对象合并', t => {
-    // 断言
-    t.is(clone(obj1, obj2), result)
+test('深度递归合并', () => {
+  expect(clone(obj1, obj2)).toEqual(result)
 })
